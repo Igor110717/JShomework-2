1ee2e6f403b301c771c548a4e9d240f6d5b5465e
Igor110717 <igorkotov110717@gmail.com>
2025-02-20 12:08:54 +0200

    Решает 2 задачу

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const logItems = function(array) {
-  for (let i = 0; i < array.length; i += 1) {
-    console.log (`${i+1} - ${array[i]}`)
-  }
-  };
+// Задание 1
+// const logItems = function(array) {
+//   for (let i = 0; i < array.length; i += 1) {
+//     console.log (`${i+1} - ${array[i]}`)
+//   }
+//   };
   
-  logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+//   logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
-  logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+//   logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+  
+  // Задание 2
+  // const calculateEngravingPrice = function(message, pricePerWord) {
+  //  const sum = pricePerWord * message.split(" ").length;
+  //  return sum
+  // };
+  
+  // console.log(
+  //   calculateEngravingPrice(
+  //     'Proin sociis natoque et magnis parturient montes mus',
+  //     10,
+  //   ),
+  // ); // 80
+  
+  // console.log(
+  //   calculateEngravingPrice(
+  //     'Proin sociis natoque et magnis parturient montes mus',
+  //     20,
+  //   ),
+  // ); // 160
+  
+  // console.log(
+  //   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+  // ); // 240
+  
+  // console.log(
+  //   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+  // ); // 120
   
